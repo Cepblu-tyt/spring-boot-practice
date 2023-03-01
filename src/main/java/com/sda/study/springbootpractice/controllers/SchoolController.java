87c1e4b92cef95c20bb0386ea8321f3272e7836e
Cepblu-tyt <sergei.oksanen@gmail.com>
2023-03-01 20:39:35 +0200

    spring-boot-practice table added

--- src/main/java/com/sda/study/springbootpractice/controllers/SchoolController.java
+++ src/main/java/com/sda/study/springbootpractice/controllers/SchoolController.java
@@ -3,6 +3,8 @@ package com.sda.study.springbootpractice.controllers;
 import com.sda.study.springbootpractice.services.SchoolService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -16,8 +18,10 @@ public class SchoolController {
     @Autowired
     private SchoolService schoolService;
 
-    public String showSchoolListPage(){
-
+    @GetMapping
+    public String showSchoolListPage(Model model){
+        model.addAttribute("schools", schoolService.findAllSchools());
+        return "school/list-school";
 
     }
 }
