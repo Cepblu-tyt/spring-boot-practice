88b03b6e972e3227a4c359e50838cbd872a710a7
Cepblu-tyt <sergei.oksanen@gmail.com>
2023-03-02 21:10:13 +0200

    spring-boot-practice HTML worked

--- src/main/java/com/sda/study/springbootpractice/controllers/SchoolController.java
+++ src/main/java/com/sda/study/springbootpractice/controllers/SchoolController.java
@@ -6,8 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 /**
  * @author Sergei Oksanen
@@ -21,7 +23,8 @@ public class SchoolController {
     private SchoolService schoolService;
 
     @GetMapping
-    public String showSchoolListPage(Model model) {
+    public String showSchoolListPage(Model model, @ModelAttribute("message") String message,
+                                     @ModelAttribute("messageType") String messageType) {
         model.addAttribute("schools", schoolService.findAllSchools());
         return "school/list-school";
     }
@@ -35,5 +38,17 @@ public class SchoolController {
             throw new RuntimeException(e);
         }
     }
-
+    @GetMapping("/delete/{id}")
+    public String deleteSchool(@PathVariable Long id, RedirectAttributes redirectAttributes){
+        try {
+            schoolService.deleteSchoolById(id);
+            redirectAttributes.addFlashAttribute("message",
+                    String.format("School(id=%d) deleted successfully",id));
+            redirectAttributes.addFlashAttribute("messageType","success");
+            return "redirect:/school";
+        } catch (SchoolNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
+
