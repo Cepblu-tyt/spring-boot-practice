8301cae9da19a6408ca8ca5b6688ca9ce6646a3d
Cepblu-tyt <sergei.oksanen@gmail.com>
2023-03-07 19:47:37 +0200

    spring-boot-practice HTML worked

--- src/main/java/com/sda/study/springbootpractice/controllers/SchoolController.java
+++ src/main/java/com/sda/study/springbootpractice/controllers/SchoolController.java
@@ -4,19 +4,17 @@ import com.sda.study.springbootpractice.exceptions.CourseNotFoundException;
 import com.sda.study.springbootpractice.exceptions.SchoolNotFoundException;
 import com.sda.study.springbootpractice.models.School;
 import com.sda.study.springbootpractice.services.SchoolService;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.Date;
 import java.util.List;
-
 
 /**
  * @author Sergei Oksanen
@@ -44,10 +42,10 @@ public class SchoolController {
 
             return new ResponseEntity<>(school, httpHeaders, HttpStatus.OK);
         }
-    }
+
 
     @GetMapping("/delete/{id}")
-    public ResponseEntity<?> deleteSchool(@PathVariable Long id,) throws SchoolNotFoundException {
+    public ResponseEntity<?> deleteSchool(@PathVariable Long id) throws SchoolNotFoundException {
         schoolService.deleteSchoolById(id);
             return new ResponseEntity<>(HttpStatus.OK);
         }
@@ -55,18 +53,32 @@ public class SchoolController {
 
     //To show the create school form page
     @PostMapping
-    public ResponseEntity<?> createSchool(@RequestBody School school) throws SchoolNotFoundException{
-        School searchSchool=schoolService.findSchoolByName(school.getName());
-        schoolService.createSchool(school);
-        return new ResponseEntity<>(HttpStatus.CREATED);
+    public ResponseEntity<?> createSchool(@RequestBody School school) {
+        try {
+            School searchSchool = schoolService.findSchoolByName(school.getName());
+            throw new RuntimeException("School already exist! Cannot create this school!");
+        } catch (SchoolNotFoundException e) {
+            schoolService.createSchool(school);
+            return new ResponseEntity<>(HttpStatus.CREATED);
+
         }
-
-
+    }
 
     @PostMapping("/update")
-    public ResponseEntity<?> updateSchool(@RequestBody School school) throws SchoolNotFoundException{
+    public ResponseEntity<?> updateSchool(@RequestBody School school) throws SchoolNotFoundException {
         schoolService.updateSchool(school);
-            return new ResponseEntity<>(HttpStatus.OK);
-        }
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @GetMapping("/restore/{id}")
+    public ResponseEntity<?> restoreSchool(@PathVariable Long id) throws SchoolNotFoundException {
+        schoolService.restoreSchoolById(id);
+
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.setContentType(MediaType.APPLICATION_JSON);
+        httpHeaders.setDate(new Date().toInstant());
+
+        return new ResponseEntity<>(httpHeaders, HttpStatus.OK);
+    }
 
 }
